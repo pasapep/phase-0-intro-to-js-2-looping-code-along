4ceda0eb2ec9b3219fe7f0bb252965ceffa7eea9
Josh <9492648646m@gmail.com>
2022-11-21 10:49:33 -0800

    commit tree 8d8efaa5579ed00961f654e65a3c28cc20a9cc27

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+
+
+
+function writeCards(array, event) { 
+    const newarray = []; 
+
+    for (let i = 0; i < array.length; i++){
+        newarray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);     
+        
+    }
+    return newarray;
+
+}
+
+function countDown () {
+    let i = 10;
+    while (i >= 0){
+        console.log (i--);
+
+
+    }
+}
